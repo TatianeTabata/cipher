88a129609ea64e44a06abbd3b553837767e6abee
TATIANE TABATA <tatit@br.ibm.com>
2019-07-15 01:31:35 -0300

    refatoração do js

--- cipher.js
+++ cipher.js
@@ -4,37 +4,24 @@ let cipher = {};
 cipher.encode = (mensagem, offset) => {
   let stringWord = "";
   let result = "";
- 
-  for(let i in mensagem) {
-  let messageAsc = mensagem.charCodeAt(i);
-        
-      if(offset <0) {  //verifica deslocamento negativo
-        if(messageAsc > 64 && messageAsc < 91) { //verifica letras maiusculas
-          messageAsc = 90 + ((((parseInt(messageAsc) - 90) + offset) % 26))
-        }
-        if(messageAsc > 96 && messageAsc < 123){ //verifica letras minusculas
-          messageAsc = 122 + ((((parseInt(messageAsc) - 122) + offset) % 26))
-      }  
-    }else 
-      
-      if(offset > 0) {
-        if(parseInt(messageAsc)==32){
-          messageAsc = 32; 
-      }
-        if(messageAsc > 64 && messageAsc < 91){
-          messageAsc = (((parseInt(messageAsc) - 65 + offset) % 26) + 65)
-      }
-        if(messageAsc > 96 && messageAsc < 123){
-          messageAsc = (((parseInt(messageAsc) - 97 + offset) % 26) + 97)
-      }    
+  for (let i in mensagem) {
+    let messageAsc = mensagem.charCodeAt(i);
+    messageAsc = parseInt(messageAsc)
+    while (offset < 0) {
+      offset += 26;
     }
-  
-    stringWord = String.fromCharCode(messageAsc);    
-    result += stringWord;  //iteracao de cada letra
+    if (messageAsc > 64 && messageAsc < 91) {
+      messageAsc = 65 + (((messageAsc - 65) + offset) % 26)
+    } else if (messageAsc > 96 && messageAsc < 123) {
+      messageAsc = (((messageAsc - 97) + offset) % 26) + 97
+    } else {
+      messageAsc === 32
+    }
+    stringWord = String.fromCharCode(messageAsc);
+    result += stringWord;
   }
-return result;
+  return result;
 }
-
 
 
 let send1 = () => {
@@ -42,47 +29,37 @@ let send1 = () => {
   deslocamento = parseInt(deslocamento);
   let message = document.getElementById("messageId").value;
   let result = cipher.encode(message, deslocamento);
-  document.getElementById("msgCifrada").innerHTML = "Mensagem cifrada é:  " + "<br />" + result;
+  document.getElementById("msgEncoded").innerHTML = result;
 }
-
 
 cipher.decode = (mensagem, offset) => {
   let stringWord = "";
   let result = "";
-
-  for(let i in mensagem) { 
+  for (let i in mensagem) {
     let messageAsc = mensagem.charCodeAt(i);
-    
-    if(offset <0) {  
-      if(messageAsc > 64 && messageAsc < 91) {
-        messageAsc = (((parseInt(messageAsc) - 65 - offset) % 26) + 65)
-      }
-      if(messageAsc > 96 && messageAsc < 123){
-        messageAsc = (((parseInt(messageAsc) - 97 - offset) % 26) + 97)
-    }  
-  }else
-    if(offset > 0) {
-      if(parseInt(messageAsc)==32){
-        messageAsc = 32; 
-      }
-      if(messageAsc > 64 && messageAsc < 91){
-        messageAsc = 90 + (((parseInt(messageAsc) - 90 - offset) % 26))  
-      }
-      if(messageAsc > 96 && messageAsc < 123){
-        messageAsc = 122 + (((parseInt(messageAsc) - 122 - offset) % 26))
-      } 
+    messageAsc = parseInt(messageAsc);
+    while (offset < 0) {
+      offset += 26;
     }
-    stringWord = String.fromCharCode(messageAsc);       
+    if (messageAsc == 32) {
+      messageAsc = 32;
+    }
+    else if (messageAsc > 64 && messageAsc < 91) {
+      messageAsc = 90 + ((messageAsc - 90 - offset) % 26)
+    }
+    else {
+      messageAsc = 122 + ((messageAsc - 122 - offset) % 26)
+    }
+    stringWord = String.fromCharCode(messageAsc);
     result += stringWord;
   }
-return result;
+  return result;
 }
 
-
-function send2(){
+function send2() {
   let deslocamento = document.getElementById("deslocId2").value;
   deslocamento = parseInt(deslocamento);
   let message = document.getElementById("messageIdDecifra").value;
   let result = cipher.decode(message, deslocamento);
-  document.getElementById("msgDecifrada").innerHTML = "Mensagem cifrada é: " + "<br />" + result;
+  document.getElementById("msgDecoded").innerHTML = result;
 }
